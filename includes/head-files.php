26406fe5840d89a22504e57257d6eaf68537e5ff
Emad Messiha <emadmessiha@gmail.com>
2019-06-03 02:17:45 -0400

    Update head-files.php

--- includes/head-files.php
+++ includes/head-files.php
@@ -2,7 +2,7 @@
 
 ?>
 <meta name="viewport" content="width=device-width">
-<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
 <link rel="stylesheet" href="/css/main.css">
 <link rel="icon" type="image/png" href="images/eweather-icon.png">
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
